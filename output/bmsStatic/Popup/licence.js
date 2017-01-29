2d98ae2348f4ad6e8eaf90d691cdcda1789de978
devops81 <devops81@gmail.com>
2017-01-29 12:59:34 +0000

    2nd commit

--- output/bmsStatic/Popup/licence.js
+++
@@ -1,6 +0,0 @@
-/******** start of licence.js ********/
-/* FlatCalendarXP Developer License registered to [openq, inc.] */
-flatkey[flatkey.length]="14699110105324411311010111211100";
-
-
-/******** end of licence.js ********/
